8f752cf8cef627ac716f2870d65cc970c2567833
lewington <lewington@student.unimelb.edu.au>
2018-03-31 15:26:09 +1100

    added pageActions

---
+++ public/assets/javascripts/actions/pageActions.js
@@ -0,0 +1,16 @@
+import dispatcher from '../dispatcher'
+
+export default {
+  toTitlePage() {
+    dispatcher.dispatch({
+      type: 'TO_TITLE_PAGE'
+    })
+  },
+
+  toDisplayePage(ball_number) {
+    dispatcher.dispatch({
+      type: 'TO_TITLE_PAGE'
+      ball_number: ball_number
+    })
+  }
+}
