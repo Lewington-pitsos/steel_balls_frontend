744e4c3d800c72de6bb0cd3ae7141a62bd673a49
lewington <lewington@student.unimelb.edu.au>
2018-04-03 09:35:50 +1000

    navigation styled

--- public/assets/javascripts/components/App/DisplayPage/Navigators/NavButton.js
+++ public/assets/javascripts/components/App/DisplayPage/Navigators/NavButton.js
@@ -8,7 +8,7 @@ export default class NavButton extends React.Component {
 
    render() {
       return (
-         <button onClick={this.props.action} className='nav-button' id={this.props.buttonId}>
+         <button onClick={this.props.action} className='button nav-button' id={this.props.buttonId}>
             {this.props.children}
          </button>
       );
