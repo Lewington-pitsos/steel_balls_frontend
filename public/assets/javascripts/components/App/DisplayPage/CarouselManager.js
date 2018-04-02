1fe371818d4edbba617f093dc1a159914bb11bf2
lewington <lewington@student.unimelb.edu.au>
2018-04-03 08:34:46 +1000

    animation plan roughly exists

--- public/assets/javascripts/components/App/DisplayPage/CarouselManager.js
+++ public/assets/javascripts/components/App/DisplayPage/CarouselManager.js
@@ -12,6 +12,13 @@ export default class CarouselManager extends React.Component {
     this.state = treeStore.getInfo()
   }
 
+  carousels() {
+    return [
+      <Carousel nodes={this.state.nodes} stateNode={this.state.atState} first={true} key={this.state.key + 1} index={this.state.index}/>,
+      <Carousel nodes={this.state.children} stateNode={!this.state.atState} first={false} key={this.state.key + 2} lastSelection={this.state.lastSelection}/>
+    ]
+  }
+
   componentWillMount() {
     treeStore.on('change', () => {
       this.setState( treeStore.getInfo() )
@@ -25,10 +32,9 @@ export default class CarouselManager extends React.Component {
           <div className='col-12'>
             <CSSTransitionGroup
               transitionName={'carousel'}
-              transitionEnterTimeout={1}
-              transitionLeaveTimeout={1}>
-              <Carousel nodes={this.state.nodes} stateNode={this.state.atState} first={true} key={this.state.key + 1} index={this.state.index}/>
-              <Carousel nodes={this.state.children} stateNode={!this.state.atState} first={false} key={this.state.key + 2} lastSelection={this.state.lastSelection}/>
+              transitionEnterTimeout={1000}
+              transitionLeaveTimeout={1000}>
+              {this.carousels()}
             </CSSTransitionGroup>
           </div>
         </div>
