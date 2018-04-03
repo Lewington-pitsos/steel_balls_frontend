29760f21a45c6cf9fc34b3842b357525b06085ee
lewington <lewington@student.unimelb.edu.au>
2018-04-03 11:10:21 +1000

    legend displays nicely

--- public/assets/javascripts/components/App/DisplayPage/CarouselManager/Carousel/CarouselNode/Ball.js
+++ public/assets/javascripts/components/App/DisplayPage/CarouselManager/Carousel/CarouselNode/Ball.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Ball extends React.Component {
   render() {
 
-    const classes = 'ball ' + this.props.category
+    const classes = 'ball ' + this.props.category + (this.props.size || '')
 
     return (
        <div className={classes}>
--- public/assets/javascripts/components/App/DisplayPage/Legend.js
+++ public/assets/javascripts/components/App/DisplayPage/Legend.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Ball from './CarouselManager/Carousel/CarouselNode/Ball'
+
 export default class Legend extends React.Component {
   constructor() {
     super()
@@ -11,6 +13,38 @@ export default class Legend extends React.Component {
          <div className='row' id='legend'>
            <div className='col-12'>
              <h3>Legend</h3>
+               <table class="table text-left">
+                <tbody>
+                  <tr valign='middle'>
+                    <td><Ball category='unknown' size=' small'/></td>
+                    <td>
+                      <h6>Unknown</h6>
+                      <p>We have no information at all about this ball. it could be any weight.</p>
+                    </td>
+                  </tr>
+                  <tr valign='middle'>
+                    <td><Ball category='possibly_lighter' size=' small'/></td>
+                    <td>
+                      <h6>Possibly Lighter</h6>
+                      <p>This ball is definitly isn't heavier than the normal weight, but it could still be lighter...</p>
+                    </td>
+                  </tr>
+                  <tr valign='middle'>
+                    <td><Ball category='possibly_heavier' size=' small'/></td>
+                    <td>
+                      <h6>Possibly Heavier</h6>
+                      <p>This ball is definitly isn't lighter than the normal weight, but it could still be heavier...</p>
+                    </td>
+                  </tr>
+                  <tr valign='middle'>
+                    <td><Ball category='normal' size=' small'/></td>
+                    <td>
+                      <h6>Normal</h6>
+                      <p>We can be sure that this ball is not the oddball.</p>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
          </div>
       );
